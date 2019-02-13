630a152f5830769284b58a23f02024a9d9113f19
Avi Schwartz <Schwartz210@gmail.com>
2019-02-12 20:58:27 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,7 +1,16 @@
 from requests import get
 
+FILES = {
+    'bot_rss_crawler.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/bot_rss_crawler.py',
+    '__init__.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/__init__.py',
+    'logger_thingy.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/logger_thingy.py',
+    'database.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/database.py',
+    'update.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/update.py'
+}
+
 
 def update_file(url, filename):
+    """Overwrites old program files with new ones pulled from Github"""
     data = get(url)
     lines = data.text.strip('\n')
     file = open(filename, 'w')
@@ -10,14 +19,12 @@ def update_file(url, filename):
     file.close()
 
 
-files = {
-    'bot_rss_crawler.py' : 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/bot_rss_crawler.py',
-    '__init__.py' : 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/__init__.py'
-}
-
-
-def run_updates():
-    for filename in files.keys():
-        url = files[filename]
+def execute():
+    """Executes script"""
+    for filename in FILES.keys():
+        url = FILES[filename]
         update_file(url, filename)
         print('updated:', filename)
+
+
+execute()
