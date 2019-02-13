97d468449b849168cfabaae25ee5ab079b6fa131
Avi Schwartz <Schwartz210@gmail.com>
2019-02-12 21:04:43 -0500

    Update bot_rss_crawler.py

--- bot_rss_crawler.py
+++ bot_rss_crawler.py
@@ -8,6 +8,7 @@ from feedparser import parse
 from bot_base_class import Bot
 from credits import bot_cred
 from database import RssDB
+from logger_thingy import logger, to_console
 
 
 class RssCrawlerBot(Bot):
@@ -17,11 +18,13 @@ class RssCrawlerBot(Bot):
         self.credential_iterator = 0
         self.db = RssDB('database.db', 'articles')
 
+    @logger
     def read_file(self):
         """Reads RSS Feed file and returns values delimited to list"""
         request = get('https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/rss_feeds.txt')
         return request.text.split('\n')
 
+    @logger
     def print_feed(self):
         """Dumps RSS data to console for testing"""
         data = self.read_file()
@@ -31,6 +34,7 @@ class RssCrawlerBot(Bot):
             for entry in feed['entries']:
                 print(entry)
 
+    @logger
     def reset_credentials(self):
         """Changes bot credentials to circumvent API posting frequency limits. A bot can only post an article
         every 10 min"""
@@ -40,6 +44,7 @@ class RssCrawlerBot(Bot):
         new_credentials = bot_cred[self.credential_iterator]
         self.set_credentials(new_credentials[0], new_credentials[1], new_credentials[2], new_credentials[3])
 
+    @logger
     def get_random_article(self):
         """Chooses random article"""
         rss_feed_list = self.read_file()
@@ -54,12 +59,15 @@ class RssCrawlerBot(Bot):
             except:
                 return self.get_random_article()
 
+    @logger
     def act(self):
         """Public main sequence"""
         while True:
             article = self.get_random_article()
             self.subreddit.submit(title=article['title'], url=article['link'])
             self.db.insert_into(article['link'])
-            print('Published:', article['title'], article['link'])
+            to_console(1, 'Published:')
+            to_console(2, article['title'])
+            to_console(2, article['link'])
             self.reset_credentials()
             sleep(Bot.MINUTE * 3.5)
