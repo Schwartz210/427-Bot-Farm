043a25f02ade11db01e52df21c74220764fe6faf
Avi Schwartz <Schwartz210@gmail.com>
2019-02-16 17:04:37 -0500

    Update update.py

--- update.py
+++ update.py
@@ -1,18 +1,31 @@
-from _thread import start_new_thread
+from requests import get
 
-from bot_rss_crawler import RssCrawlerBot
-from update import update_local_source_code
-
-def execute():
-    start_new_thread(update_local_source_code, ())
-    feed_list = 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/rss_feeds.txt'
-    sleep_phase = RssCrawlerBot.MINUTE * 15
-    bot = RssCrawlerBot(feed_list, sleep_phase, 'rssbot')
-    while True:
-        bot.act()
+FILES = {
+    'bot_rss_crawler.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/bot_rss_crawler.py',
+    'go.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/__init__.py',
+    'logger_thingy.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/logger_thingy.py',
+    'database.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/database.py',
+    'update.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/update.py',
+    'bot_base_class.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/bot_base_class.py'
+}
 
 
-if __name__ == '__main__':
-    execute()
+def update_file(url, filename):
+    """Overwrites old program files with new ones pulled from Github"""
+    data = get(url)
+    lines = data.text.strip('\n')
+    file = open(filename, 'w')
+    for line in lines:
+        file.write(line)
+    file.close()
+
+
+def update_local_source_code():
+    """Executes script"""
+    for filename in FILES.keys():
+        url = FILES[filename]
+        update_file(url, filename)
+        print('updated:', filename)
+
 
 
