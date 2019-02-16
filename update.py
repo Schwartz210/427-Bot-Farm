f044f74364fe735e6b7a7933522f0df483e2d465
Avi Schwartz <Schwartz210@gmail.com>
2019-02-16 12:19:07 -0500

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ from requests import get
 
 FILES = {
     'bot_rss_crawler.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/bot_rss_crawler.py',
-    '__init__.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/__init__.py',
+    'go.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/__init__.py',
     'logger_thingy.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/logger_thingy.py',
     'database.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/database.py',
     'update.py': 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/update.py'
