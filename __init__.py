9b0f52ce0cc9ffe51739b3fffafee0d3735ed2ff
Avi Schwartz <Schwartz210@gmail.com>
2019-02-16 13:13:36 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,9 @@ from bot_rss_crawler import RssCrawlerBot
 
 
 def execute():
-    bot = RssCrawlerBot()
+    feed_list = 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/rss_feeds.txt'
+    sleep_phase = RssCrawlerBot.MINUTE * 15
+    bot = RssCrawlerBot(feed_list, sleep_phase)
     bot.act()
 
 
