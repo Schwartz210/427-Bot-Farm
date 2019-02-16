cbeee1d0817aef861a24f4b24a424b9074959102
Avi Schwartz <Schwartz210@gmail.com>
2019-02-16 16:52:05 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,54 +1,28 @@
 from sqlite3 import connect
 
 
-class DB(object):
+class DB:
     def __init__(self, file, table):
         self.FILE = file
         self.TABLE = table
 
-    def insert_into(self, record):
-        """Abstract method"""
-        pass
-
-    def contains(self, value):
-        """Abstract method"""
-        pass
-
-    def dump(self):
-        """Prints contents of DB to console"""
-        conn = connect(self.FILE)
-        c = conn.cursor()
-        sql = 'SELECT * FROM %s' % self.TABLE
-        data = list(c.execute(sql))
-        conn.commit()
-        conn.close()
-        for record in data:
-            print(record)
-
-
-class RssDB(DB):
-    def __init__(self, file, table):
-        DB.__init__(self, file, table)
-
-    def insert_into(self, url):
+    def insert_into(self, sub, url):
         """Commits single record to DB"""
         conn = connect(self.FILE)
         c = conn.cursor()
-        sql = 'INSERT INTO %s VALUES("%s")' % (self.TABLE, url)
+        sql = 'INSERT INTO %s VALUES("%s", "%s")' % (self.TABLE, sub, url)
         c.execute(sql)
         conn.commit()
         conn.close()
 
-    def contains(self, url):
+    def contains(self, sub, url):
         """Evaluates if url is in DB. Returns bool"""
         conn = connect(self.FILE)
         c = conn.cursor()
-        sql = 'SELECT * FROM %s WHERE url="%s"' % (self.TABLE, url)
+        sql = 'SELECT * FROM %s WHERE subreddit="%s" AND url="%s"' % (self.TABLE, sub, url)
         data = list(c.execute(sql))
         conn.close()
         if len(data) > 0:
             return True
         else:
             return False
-
-
