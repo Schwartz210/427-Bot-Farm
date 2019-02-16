55f9855452e0a9961b94f633abf9ba843004d2c2
Avi Schwartz <Schwartz210@gmail.com>
2019-02-16 16:53:23 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,16 @@
-from bot_rss_crawler import RssCrawlerBot
+from _thread import start_new_thread
 
+from bot_rss_crawler import RssCrawlerBot
+from update import update_local_source_code
 
 def execute():
+    start_new_thread(update_local_source_code, ())
     feed_list = 'https://raw.githubusercontent.com/Schwartz210/427-Bot-Farm/master/rss_feeds.txt'
     sleep_phase = RssCrawlerBot.MINUTE * 15
-    bot = RssCrawlerBot(feed_list, sleep_phase)
-    bot.act()
+    bot = RssCrawlerBot(feed_list, sleep_phase, 'rssbot')
+    while True:
+        bot.act()
 
 
 if __name__ == '__main__':
     execute()
-
-
