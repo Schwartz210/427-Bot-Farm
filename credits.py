2bc035b486018551696a1b902470f25b89461a17
Avi Schwartz <Schwartz210@gmail.com>
2019-02-18 10:37:22 -0500

    Create credits.py

---
+++ credits.py
@@ -0,0 +1,7 @@
+"""Credentials hidden"""
+
+bot_cred = [
+  ['username', 'password', 'secret key 1', 'secret key 2'],
+  ['username', 'password', 'secret key 1', 'secret key 2'],
+  ['username', 'password', 'secret key 1', 'secret key 2']
+]
