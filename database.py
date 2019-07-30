8f1969e0847249a97b6c9404a74a59146762ccc7
Avi Schwartz <Schwartz210@gmail.com>
2019-07-29 21:01:06 -0400

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlite3 import connect
+from gg import out
 
 
-class DB:
+class DB(object):
     def __init__(self, file, table):
         self.FILE = file
         self.TABLE = table
@@ -12,17 +13,36 @@ class DB:
         c = conn.cursor()
         sql = 'INSERT INTO %s VALUES("%s", "%s")' % (self.TABLE, sub, url)
         c.execute(sql)
+        out('after execute')
         conn.commit()
         conn.close()
 
     def contains(self, sub, url):
         """Evaluates if url is in DB. Returns bool"""
+        out('contains')
         conn = connect(self.FILE)
         c = conn.cursor()
         sql = 'SELECT * FROM %s WHERE subreddit="%s" AND url="%s"' % (self.TABLE, sub, url)
+        out(sql)
         data = list(c.execute(sql))
+        out('after execute')
         conn.close()
         if len(data) > 0:
+            out('true')
             return True
         else:
+            out('false')
             return False
+
+    def print_fields(self):
+        conn = connect(self.FILE)
+        c = conn.execute('SELECT * FROM articles5')
+        print(c.description)
+
+    def dump(self):
+        conn = connect(self.FILE)
+        c = conn.execute('SELECT * FROM articles5')
+        for record in c:
+            print(record)
+
+
